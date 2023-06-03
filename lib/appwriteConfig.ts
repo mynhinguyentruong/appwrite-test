c8bd7f53d47577d08e3e845fcf92acfd853cdad6
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-03 12:20:43 -0400

    Add database appwrite config

--- lib/appwriteConfig.ts
+++ lib/appwriteConfig.ts
@@ -1,7 +1,9 @@
-import { Client, Account, } from 'appwrite';
+import { Client, Account, Databases} from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
     .setProject('64749ba6eade18e58a13');               // Your project ID
 
 export const account = new Account(client);
+
+export const databases = new Databases(client)
