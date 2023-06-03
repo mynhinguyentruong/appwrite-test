c95b93a979966f274051577e36d309653ffeddd0
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-03 14:30:39 -0400

    Add sidebar ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,71 @@
-export default function Page() {
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+
+import {account} from "#/lib/appwriteConfig";
+import {useEffect, useState} from "react";
+import GlobalNav from "#/ui/global-nav";
+
+export interface IParams {
+    userId?: string,
+    secret?: string
+}
+
+export default function Page({params}: {params: IParams} ) {
+    const router = useRouter();
+    const searchParams = useSearchParams()
+
+    const userId = searchParams.get('userId')
+    const secret = searchParams.get('secret')
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    useEffect(() => {
+        if (userId && secret) {
+            const promise = account.updateMagicURLSession(userId, secret);
+
+            promise
+                .then(function (response) {
+                    console.log("New session created: ")
+                    console.log(response); // Success
+                    const {userId, $id} = response
+                    console.log({userId} )
+
+                    setIsAuthenticated(true)
+                })
+                .catch(error => {
+                    console.log("Error when update URL SESSION")
+                    console.log(error)
+                    router.push('/login')
+                })
+        } else {
+            const promise = account.get()
+            promise
+                .then(function (response) {
+                    console.log("Success getting session")
+                    console.log(response); // Success
+                    // Permission.write(Role.user(response.$id, 'verified'))
+                    setIsAuthenticated(true)
+                })
+                .catch(error => {
+                    console.log("Error while trying to get session")
+                    console.log({error})
+
+                    router.push('/login')
+                });
+
+        }
+    }, [isAuthenticated])
+
+
     return (
-        <>My Dashboard</>
+        isAuthenticated &&
+        <div className="flex ">
+            <GlobalNav />
+            <aside>asdsad</aside>
+        </div>
+
     )
+
 }
