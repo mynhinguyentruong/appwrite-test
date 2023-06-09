b8cb4f59f31d4da2358b942e9a75100dde933c92
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-09 16:23:14 -0400

    Move logic to create new post to server side

---
+++ app/api/new-post/route.ts
@@ -0,0 +1,66 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import {
+  ID,
+  Client,
+  Storage,
+  Databases,
+  Permission,
+  Role,
+} from "node-appwrite";
+// Init SDK
+const client = new Client();
+
+const storage = new Storage(client);
+const databases = new Databases(client);
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
+  .setProject("64749ba6eade18e58a13") // Your project ID
+  .setKey(process.env.APPWRITE_API_KEY as string); // Your secret API key
+
+export async function POST(request: NextRequest) {
+  // create post
+  const resBody = await request.json();
+
+  const post = await databases.createDocument(
+    process.env.DATABASE_ID as string,
+    process.env.POST_COLLECTION_ID as string,
+    ID.unique(),
+    resBody,
+    [
+      Permission.update(Role.users()),
+      Permission.write(Role.users()),
+      Permission.read(Role.users()),
+    ]
+  );
+
+  if (resBody.hasPhoto) {
+    const bucket = await storage.createBucket(
+      post.$id,
+      post.$id,
+      [
+        Permission.create(Role.users()),
+        Permission.write(Role.users()),
+        Permission.read(Role.any()),
+      ],
+      true,
+      true
+    );
+  }
+
+  return NextResponse.json({ postId: post.$id });
+
+  // if hasphoto, upload file
+}
+
+// post create document
+
+// post create bucket if hasphoto
+
+// return the bucketId to the client
+
+// client: bucketId to make req upload file
+
+// on all file upload success, make req to query all post
+//return NextResponse.json({ message: "success" });
