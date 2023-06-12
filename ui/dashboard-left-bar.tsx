11811f07cfefe3623563e302f16bf551ce01e159
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-12 13:28:48 -0400

    update active link style

--- ui/dashboard-left-bar.tsx
+++ ui/dashboard-left-bar.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import Link from "next/link";
-import Image from "next/image";
-import UserProfile from "#/ui/user-profile";
-import { Models } from "appwrite";
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
+
 
 export default function DashboardLeftBar() {
+  const pathname = usePathname()
 
     return (
         <div className="w-full lg:w-1/4 pl-4 lg:pl-0 pr-6 mt-8 mb-4">
@@ -31,26 +34,15 @@ export default function DashboardLeftBar() {
                   </svg>
                 Home
                 </Link>
-                <Link href="/dashboard" className="text-blue-300 mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300">
+                <Link href="/dashboard" className={clsx((pathname === '/dashboard') && 'text-blue-300', ' mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300')}>
                   <svg className="mr-4 h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"></path></svg>
                   
                   Dashboard
                 </Link>
-                <Link href="/notifications" className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300">
-                  <svg className="mr-4 h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
-                Notifications
-                </Link>
-                <Link href="/messages" className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300">
-                  <svg className="mr-4 h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                  Messages
-                </Link>
-                <Link href="/bookmarks" className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300">
-                  <svg className="mr-4 h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
-                  Bookmarks
-                </Link>
-                <Link href="/lists" className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300">
+              
+                <Link href="/media" className={clsx((pathname === '/media') && 'text-blue-300', ' mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300')}>
                   <svg className="mr-4 h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
-                  Lists
+                  Media
                 </Link>
                     <Link href="/profile" className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:border hover:border-gray-300 hover:text-blue-300">
                   <svg className="mr-4 h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
