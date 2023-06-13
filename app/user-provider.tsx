c105a024a6afb726f80fb79e879230a4929b0acf
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-13 11:06:42 -0400

    Add user provider

---
+++ app/user-provider.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { account } from "#/lib/appwriteConfig"
+import { Models } from "appwrite"
+
+import { ReactNode, createContext, useEffect, useState } from "react"
+
+const UserContext = createContext<null | User>(null)
+
+type User = {
+    user: Models.User<Models.Preferences> | null
+}
+
+export default function UserProvider({children}: {children: ReactNode}) {
+    const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null)
+
+    const getAccount = async () => {
+        const user = await account.get()
+
+        if (!user) return setUser(null)
+
+        setUser(user)
+    }
+
+    useEffect(() => {
+        getAccount()
+    }, [])
+
+    return (
+        <UserContext.Provider value={{user}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
