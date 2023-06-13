1f803328ad803c10aeb22fe1bb066a48f03e0f72
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-13 08:10:24 -0400

    UI update

--- ui/post.tsx
+++ ui/post.tsx
@@ -109,20 +109,14 @@ export default function Post({post, user, likePost}: {post: Models.Document, use
                       
                         {/* SVG */}
                         <div className="flex items-center">
-                            <div className="flex-1 text-center">
+                            {/* <div className="flex text-center">
                                 <a href="#" className="w-12 mt-1 group flex items-center dark:text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full dark:hover:bg-gray-800 hover:text-blue-300">
                                     <svg className="text-center h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
                                   </a>
-                            </div>
-
-                            <div className="flex-1 text-center py-2 m-2">
-                                <a href="#" className="w-12 mt-1 group flex items-center dark:text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full dark:hover:bg-gray-800 hover:text-blue-300">
-                                    <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"></path></svg>
-                                </a>
-                            </div>
+                            </div> */}
 
                             {/* HEART */}
-                            <div className="flex-1 text-center py-2 m-2">
+                            <div className="flex text-center py-2 m-2">
                                 <div className="flex items-center justify-center">
                                 <button onClick={() => likePost(post.$id, user?.$id)} className="w-12 mt-1 group flex items-center dark:text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full dark:hover:bg-gray-800 hover:text-blue-300">
                                     <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
@@ -132,21 +126,7 @@ export default function Post({post, user, likePost}: {post: Models.Document, use
                                 </div>
                             </div>
 
-                            <div className="flex-1 text-center py-2 m-2">
-                                <a href="#" className="w-12 mt-1 group flex items-center dark:text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full dark:hover:bg-gray-800 hover:text-blue-300">
-                                    <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>
-                            </a>
-                            </div>
-                            <div className="flex-1 text-center py-2 m-2">
-                                <a href="#" className="w-12 mt-1 group flex items-center dark:text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full dark:hover:bg-gray-800 hover:text-blue-300">
-                                    <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20"></path></svg>
-                            </a>
-                            </div>
-                            <div className="flex-1 text-center py-2 m-2">
-                                <a href="#" className="w-12 mt-1 group flex items-center dark:text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full dark:hover:bg-gray-800 hover:text-blue-300">
-                                    <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
-                            </a>
-                            </div>
+                           
                         </div>
                         <div><Link href={`/post/${post.$id}`} className="text-teal">Show this thread</Link></div>
 
