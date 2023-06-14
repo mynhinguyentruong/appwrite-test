c1c596a2fc28bbc616436feb0359d95b5d18f910
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-13 20:15:22 -0400

    code clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { account } from "#/lib/appwriteConfig";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link';
@@ -8,7 +8,6 @@ import Link from 'next/link';
 export default function Home() {
 
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
 
     useEffect(() => {
         const fallbackCookies = localStorage.getItem('cookieFallback');
